44cb69eba3a3b554c03eab7e8ce855186f6523f1
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-05-19 21:35:25 +0530

    tested main functions of authentication

--- user_control/tests.py
+++ user_control/tests.py
@@ -3,27 +3,52 @@ from rest_framework.test import APITestCase
 from .views import get_random,get_access_token,get_refresh_token
 # Create your tests here.
 
-class TestGenericFunctions(APITestCase):
-    def test_get_randon(self):
-        rand1 = get_random(10)
-        rand2 = get_random(10)
-        rand3 = get_random(15)
-        ## to check if it sends any value or not
-        self.assertTrue(rand1)
+# class TestGenericFunctions(APITestCase):
+#     def test_get_randon(self):
+#         rand1 = get_random(10)
+#         rand2 = get_random(10)
+#         rand3 = get_random(15)
+#         ## to check if it sends any value or not
+#         print("Testing get random function")
+#         self.assertTrue(rand1)
 
-        # to check the equality of 2 values
+#         # to check the equality of 2 values
 
-        self.assertEqual(len(rand1),10)
+#         self.assertEqual(len(rand1),10)
 
-    def test_get_access_token(self):
+#     def test_get_access_token(self):
+#         payload = {
+#             "id":1,
+
+#         }
+#         token =get_access_token(payload)
+#         print("Testing get get_access_token function")
+
+#         self.assertTrue(token)
+
+#     def test_get_refresh_token(self):
+        
+#         token =get_refresh_token()
+#         print("Testing get get_refresh_token function")
+#         self.assertTrue(token)
+
+class TestFunctions(APITestCase):
+    login_url = "/user/login/"
+    register_url = "/user/register/"
+    refresh_url = "user/refresh/"
+    def Test_Login(self):
         payload = {
-            "id":1,
+            "username":"admin",
+            "password":"1898"
 
         }
-        token =get_access_token(payload)
-        self.assertTrue(token)
+        response = self.client.post(self.login_url,data = payload)
+        self.assertEqual(response.status_code,5000)
 
-    def test_get_refresh_token(self):
-        
-        token =get_refresh_token()
-        self.assertTrue(token)
+    def Test_Register(self):
+        payload = {
+            "username":"newadmin",
+            "password":"18987374"
+        }
+        response = self.client.post(self.register_url,data = payload)
+        self.assertEqual(response.status_code,201)
